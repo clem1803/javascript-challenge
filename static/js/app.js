d9b2b02887be20672166891d672b94b9aa275286
clem1803 <clementboit@gmail.com>
2021-05-21 09:22:17 -0400

    minor adjustment

--- static/js/app.js
+++ static/js/app.js
@@ -12,6 +12,8 @@ var button = d3.select("#filter-btn");
 var input = d3.select("#datetime");
 var tBody = d3.select("tbody");
 var inputCity = d3.select("#cityy");
+var inputState = d3.select("#state");
+var inputCountry = d3.select("#country");
 
 // Create event handlers 
 button.on("click", runEnter);
@@ -23,9 +25,17 @@ function runEnter() {
 
     var inputCityValue = inputCity.property("value");
 
-    var filteredData = tableData.filter(ufo => Date.parse(ufo.datetime) >= Date.parse(inputValue) || ufo.city === inputCityValue);
+    var inputStateValue = inputState.property("value");
+
+    var inputCountryValue = inputCountry.property("value");
+
+    var filteredData = tableData.filter(ufo => Date.parse(ufo.datetime) >= Date.parse(inputValue) || ufo.city === inputCityValue)
+        || ufo.state === inputStateValue;
 
     console.log(filteredData);
+
+    var filteredData2 = tableData.filter(ufo => ufo.country === inputCountryValue);
+    console.log(filteredData2)
 
     tBody.html("");
     filteredData.forEach(
